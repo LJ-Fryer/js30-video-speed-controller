e948f5022afbd512945e52ab3d8fe926b9869bc4
LJ-Fryer <ljf_aus@hotmail.com>
2025-06-12 17:05:18 +1000

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Testing");
+const speed = document.querySelector(".speed");
+const bar = speed.querySelector(".speed-bar"); // 0.5 to 4x speed
+const video = document.querySelector(".flex");
+
+function handleRate(e) {
+  const y = e.pageY - this.offsetTop;
+  const percent = y / this.offsetHeight;
+  const min = 0.5;
+  const max = 4;
+  const height = Math.round(percent * 100) + "%";
+  bar.style.height = height;
+
+  const playbackRate = (max - min) * percent + min;
+  bar.textContent = playbackRate.toFixed(1) + "x";
+  video.playbackRate = playbackRate;
+}
+
+speed.addEventListener("mousemove", handleRate);
